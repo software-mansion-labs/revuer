98683138b2229340bcd183f27d282bafc533bef9
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-02-01 21:17:09 +0100

    implement github-repo-url

--- src/github/github-repo-url.test.ts
+++ src/github/github-repo-url.test.ts
@@ -1,3 +1,38 @@
-import {} from './github-repo-url'
+import { Url } from '~/core'
+import {
+  extractOrganizationName,
+  extractRepositoryName,
+  validateGitHubRepoLink,
+} from './github-repo-url'
 
-it('should do sth', () => {})
+it('should detect valid GitHub repo link', () => {
+  const result = validateGitHubRepoLink(
+    new Url('https://github.com/ORGANIZATION_NAME/REPO_NAME')
+  )
+
+  expect(result).toBe(true)
+})
+
+it('should detect invalid GitHub repo link', () => {
+  const result = validateGitHubRepoLink(
+    new Url('https://github.com/ORGANIZATION_NAME')
+  )
+
+  expect(result).toBe(false)
+})
+
+it('should extract organization name from a valid link', () => {
+  const result = extractOrganizationName(
+    new Url('https://github.com/ORGANIZATION_NAME/REPO_NAME')
+  )
+
+  expect(result).toBe('ORGANIZATION_NAME')
+})
+
+it('should extract repo name from a valid link', () => {
+  const result = extractRepositoryName(
+    new Url('https://github.com/ORGANIZATION_NAME/REPO_NAME')
+  )
+
+  expect(result).toBe('REPO_NAME')
+})
--- src/github/github-repo-url.ts
+++ src/github/github-repo-url.ts
@@ -1,14 +1,28 @@
-import { Url } from '~/core'
+import { AppError, Url } from '~/core'
+
+const GITHUB_REPO_REGEX = /^https:\/\/github.com\/([\w-]+)\/([\w-]+)\/?$/
 
 export function validateGitHubRepoLink(url: Url) {
-  // TODO
-  return true
+  if (!url.value) return false
+  return GITHUB_REPO_REGEX.test(url.value)
 }
 
 export function extractOrganizationName(url: Url) {
-  return ''
+  if (!url.value)
+    throw new AppError("Couldn't extract organization name from empty string")
+  const matches = url.value.match(GITHUB_REPO_REGEX)
+  if (!matches || matches.length != 3) {
+    throw new AppError("Couldn't extract organization name")
+  }
+  return matches[1]
 }
 
 export function extractRepositoryName(url: Url) {
-  return ''
+  if (!url.value)
+    throw new AppError("Couldn't extract repository name from empty string")
+  const matches = url.value.match(GITHUB_REPO_REGEX)
+  if (!matches || matches.length != 3) {
+    throw new AppError("Couldn't extract repository name")
+  }
+  return matches[2]
 }
