8171b71ea1514047759ed8d19a3b6e24f17405ad
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-02-01 15:46:28 +0100

    generate github gql

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+import { config as configDotEnv } from 'dotenv'
+
+configDotEnv()
+
+const config: CodegenConfig = {
+  schema: './github-schema.graphql',
+  documents: ['src/**/*.tsx'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    './src/generated/gql/': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
+}
+
+export default config
