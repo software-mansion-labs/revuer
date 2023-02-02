c99ae23bd71ac2032298ce08c91f9cb63035a9b4
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-02-03 00:27:21 +0100

    use table

--- src/stats/reviewers-statistics.tsx
+++ src/stats/reviewers-statistics.tsx
@@ -1,8 +1,77 @@
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
 import { FC } from 'react'
 import { ReviewerStatistic } from './code-review-stats-use-case'
+
+const columnHelper = createColumnHelper<ReviewerStatistic>()
+
+const columns = [
+  columnHelper.accessor('author.username', {
+    // cell(info) {
+    //   return <Text value='x' />
+    // },
+  }),
+]
 
 export const ReviewersStatisticsTable: FC<{
   statistics: ReviewerStatistic[]
 }> = ({ statistics }) => {
-  return <div>{JSON.stringify(statistics, undefined, 4)}</div>
+  const table = useReactTable({
+    data: statistics,
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        {table.getFooterGroups().map((footerGroup) => (
+          <tr key={footerGroup.id}>
+            {footerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.footer,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
+  )
+  // return <div>{JSON.stringify(statistics, undefined, 4)}</div>
 }
